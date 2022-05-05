7683e7a78e180c76011d551a2d0ca557bf4ed1a7
PB-vt <cv.juanp@gmail.com>
2022-05-05 13:02:20 -0600

    Added main App components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react'
-import logo from './logo.svg'
+/* eslint-disable @typescript-eslint/no-empty-interface */
+import React, { Component } from 'react'
+import Header from './components/Header'
 import './App.css'
+import Footer from './components/Footer'
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+interface State {}
+
+class App extends Component<State> {
+  constructor(props: string) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <>
+        <Header />
+        <div id='main'>
+          {/* <CvForm/>  */}
+          {/* <CvTemplate/> */}
+        </div>
+        <Footer />
+      </>
+    )
+  }
 }
 
 export default App
---
+++ src/components/CvForm.tsx
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+
+interface Props {
+  personalInfo: () => void
+}
+
+export default class CvForm extends PureComponent<Props> {
+  render() {
+    return <div>CvForm</div>
+  }
+}
