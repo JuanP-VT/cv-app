54f2356763e6148c5aad579de11d273da128d830
PB-vt <cv.juanp@gmail.com>
2022-05-07 15:14:23 -0600

    Moved all inputs into new module components

--- src/components/CvForm.tsx
+++ src/components/CvForm.tsx
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react'
-import PhotoInput from './photoInput'
+import FormPersonalInfo from './CvFormComponents/FormPersonalInfo'
 interface Props {
   saveInAppState: (objProp: string, id: string) => void
   savePhotoSrc: () => void
@@ -9,25 +9,10 @@ export default class CvForm extends PureComponent<Props> {
   render() {
     return (
       <div id='cvFormContainer'>
-        <input
-          id='firstName'
-          type='text'
-          placeholder='First Name'
-          onChange={() => this.props.saveInAppState('firstName', 'firstName')}
+        <FormPersonalInfo
+          saveInAppState={this.props.saveInAppState}
+          savePhotoSrc={this.props.savePhotoSrc}
         />
-        <input
-          id='lastName'
-          type='text'
-          placeholder='Last Name'
-          onChange={() => this.props.saveInAppState('lastName', 'lastName')}
-        />
-        <input
-          id='title'
-          type='text'
-          placeholder='Title'
-          onChange={() => this.props.saveInAppState('title', 'title')}
-        />
-        <PhotoInput savePhotoSrc={this.props.savePhotoSrc} />
       </div>
     )
   }
---
+++ src/components/CvFormComponents/FormPersonalInfo.tsx
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+import PhotoInput from '../photoInput'
+
+interface Props {
+  saveInAppState: (objProp: string, id: string) => void
+  savePhotoSrc: () => void
+}
+
+class FormPersonalInfo extends Component<Props> {
+  state = {}
+
+  render() {
+    return (
+      <>
+        {' '}
+        <input
+          id='firstName'
+          type='text'
+          placeholder='First Name'
+          onChange={() => this.props.saveInAppState('firstName', 'firstName')}
+        />
+        <input
+          id='lastName'
+          type='text'
+          placeholder='Last Name'
+          onChange={() => this.props.saveInAppState('lastName', 'lastName')}
+        />
+        <input
+          id='title'
+          type='text'
+          placeholder='Title'
+          onChange={() => this.props.saveInAppState('title', 'title')}
+        />
+        <PhotoInput savePhotoSrc={this.props.savePhotoSrc} />
+        <input
+          id='address'
+          type='text'
+          placeholder='address'
+          onChange={() => this.props.saveInAppState('address', 'address')}
+        />
+        <input
+          id='phoneNumber'
+          type='text'
+          placeholder='Phone Number'
+          onChange={() => this.props.saveInAppState('phoneNumber', 'phoneNumber')}
+        />
+        <input
+          id='email'
+          type='text'
+          placeholder='Email'
+          onChange={() => this.props.saveInAppState('phoneNumber', 'phoneNumber')}
+        />
+        <input
+          id='description'
+          type='text'
+          placeholder='Description'
+          onChange={() => this.props.saveInAppState('description', 'description')}
+        />
+      </>
+    )
+  }
+}
+
+export default FormPersonalInfo
