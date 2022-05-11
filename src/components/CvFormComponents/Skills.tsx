1d1eff16f397826b9a11bfcb6dfd04d258506174
PB-vt <cv.juanp@gmail.com>
2022-05-11 10:53:43 -0600

    Added maximum possible skills to 9

--- src/components/CvFormComponents/Skills.tsx
+++ src/components/CvFormComponents/Skills.tsx
@@ -17,11 +17,23 @@ class Skills extends Component<Props> {
     const hasSkill1 = this.props.appState.hasSkill1
     const hasSkill2 = this.props.appState.hasSkill2
     const hasSkill3 = this.props.appState.hasSkill3
+    const hasSkill4 = this.props.appState.hasSkill4
+    const hasSkill5 = this.props.appState.hasSkill5
+    const hasSkill6 = this.props.appState.hasSkill6
+    const hasSkill7 = this.props.appState.hasSkill7
+    const hasSkill8 = this.props.appState.hasSkill8
+    const hasSkill9 = this.props.appState.hasSkill9
     return (
       <div id='templateSkills'>
         {hasSkill1 && <FormSkill appState={this.props.appState} refNum='1' refInState='skill1' />}
         {hasSkill2 && <FormSkill appState={this.props.appState} refNum='2' refInState='skill2' />}
         {hasSkill3 && <FormSkill appState={this.props.appState} refNum='3' refInState='skill3' />}
+        {hasSkill4 && <FormSkill appState={this.props.appState} refNum='4' refInState='skill4' />}
+        {hasSkill5 && <FormSkill appState={this.props.appState} refNum='5' refInState='skill5' />}
+        {hasSkill6 && <FormSkill appState={this.props.appState} refNum='6' refInState='skill6' />}
+        {hasSkill7 && <FormSkill appState={this.props.appState} refNum='7' refInState='skill7' />}
+        {hasSkill8 && <FormSkill appState={this.props.appState} refNum='8' refInState='skill8' />}
+        {hasSkill9 && <FormSkill appState={this.props.appState} refNum='9' refInState='skill9' />}
 
         <button id='addSkill' onClick={this.props.handleAddSkill}>
           Add Skill
