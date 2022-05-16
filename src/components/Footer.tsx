14ae76815eccb502b66708522ed2654fa9208a00
PB-vt <cv.juanp@gmail.com>
2022-05-16 15:09:27 -0600

    added footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,16 @@
 import React, { Component } from 'react'
-
+import gitHubIcon from './img/demo/github.png'
 export default class Footer extends Component {
   render() {
-    return <div id='footer'>Footer</div>
+    return (
+      <div id='footer'>
+        <div className='footerIcon'>
+          <img id='footerIcon' src={gitHubIcon} alt='gitHub Icon' />
+        </div>
+        <div className='footerPb'>
+          <a href='https://github.com/JuanP-VT'> Made with ❤️ by Juan P</a>
+        </div>
+      </div>
+    )
   }
 }
